3af01b194f53af90e9b78592e620b1a2181b161f
SiddharthSai4701 <sidvsai@gmail.com>
2024-08-31 16:32:22 +0530

    Trial

--- server.js
+++ server.js
@@ -13,3 +13,8 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+app.listen(3000, () => {
+    console.log("RANDOM")
+});
